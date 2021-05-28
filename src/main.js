9c8e1c5b108ab649186ece52c361ae5381740946
GongNextDoor <1582016240@qq.com>
2021-05-28 17:04:07 +0800

    多余文件删除

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 
 /** 插件 start */
 import Element from 'element-ui' // ElementUI
@@ -15,6 +14,5 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: (h) => h(App)
 }).$mount('#app')
